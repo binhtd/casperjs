1776c6eecff7cd68193785724d08c167b21b308c
binhtd <trinhducbinh@gmail.com>
2016-08-26 18:01:00 +0700

    update code

--- crawler2.js
+++ crawler2.js
@@ -20,16 +20,6 @@ var terminate = function() {
     this.echo("Exiting..").exit();
 };
 
-
-var processPage = function() {
-    jobs = this.evaluate(getJobs);
-    require('utils').dump(jobs);
-
-    if (currentPage >= 3 || !this.exists("table#jobs")) {
-        return terminate.call(casper);
-    }
-};
-
 casper.start(url);
 casper.then(function(){
     this.mouse.click("label[for='gas']");
@@ -49,11 +39,11 @@ casper.wait(10000, function() {
 
 casper.wait(10000, function() {
     this.mouse.click("#select2-number_person-container");
-    this.mouse.click("#select2-number_person-results");
+    this.mouse.click("#select2-number_person-results li:first-child");
 
 
     this.mouse.click("#select2-number_room-container");
-    this.mouse.click("#select2-number_room-results");
+    this.mouse.click("#select2-number_room-results li:first-child");
 
     this.mouse.click("#spaceheat-appl-group input[value='none']");
     this.mouse.click("#cloth-dryer-group label[for='dryer-no']");
@@ -64,7 +54,20 @@ casper.wait(10000, function() {
 });
 
 casper.wait(10000, function() {
-    casper.capture("vic.png");
+    this.thenOpen("https://compare.switchon.vic.gov.au/service/offers", {
+        method: 'get',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+    });
+});
+
+casper.then(function(){
+    var fs = require('fs');
+    var f = fs.open('./parse_result.txt', 'w');
+    f.write(JSON.parse(this.getPageContent()));
+    f.close();
 });
 
 casper.run();
