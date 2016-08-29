6176a858d6a43c1b08cd6e2d8756bb8da3554d24
binhtd <trinhducbinh@gmail.com>
2016-08-29 17:16:23 +0700

    draft version of parse home gas

--- crawler2.js
+++ crawler2.js
@@ -7,11 +7,16 @@ var x = require('casper').selectXPath,
     utils = require('utils'),
     url = 'https://compare.switchon.vic.gov.au',
     offerList = [],
-    gasHomePostcodeList = [3011, 3953, 3179, 3141, 3199];
+    gasHomePostcodeList = [3011, 3953, 3179, 3141, 3199],
+    electricHomePostcodeList = [3000, 3011, 3944, 3284, 3841],
+    current = 0,
+    end = 0;
 
 var casper = require("casper").create({
-    waitTimeout: 10000,
-    stepTimeout:10000,
+    verbose: true,
+    logLevel: "debug",
+    waitTimeout: 100000,
+    stepTimeout:100000,
     pageSettings: {
         loadImages: false,
         loadPlugins: false,
@@ -28,58 +33,69 @@ casper.saveJSON = function(what) {
 };
 
 casper.start(url);
-casper.each(gasHomePostcodeList, function(self, gasHomepPostcode){
 
-});
-    casper.then(function(){
-        this.mouse.click("label[for='gas']");
-        this.mouse.click("label[for='home']");
-        this.mouse.click("label[for='home-shift']");
-        this.sendKeys("input[name='postcode']", casper.page.event.key.Enter , {keepFocus: true});
-        this.sendKeys("input[name='postcode']", "3011");
-        this.click("#postcode-btn");
+//start parse for home gas
+end = gasHomePostcodeList.length;
+for (;current < end;) {
+    (function(cntr) {
+        casper.then(function() {
+            this.mouse.click("label[for='gas']");
+            this.mouse.click("label[for='home']");
+            this.mouse.click("label[for='home-shift']");
+            this.sendKeys("input[name='postcode']", casper.page.event.key.Enter , {keepFocus: true});
+            this.sendKeys("input[name='postcode']", gasHomePostcodeList[cntr] + "");
+            this.click("#postcode-btn");
+            this.capture("png" + gasHomePostcodeList[cntr] + ".png");
 
-        this.wait(5000, function() {
-            this.click("label[for='energy-concession-no']");
-            this.click("#disclaimer_chkbox");
-            this.click("#btn-proceed");
-        });
-
-        this.wait(5000, function() {
-            this.mouse.click("#select2-number_person-container");
-            this.mouse.click("#select2-number_person-results li:first-child");
-
-            this.mouse.click("#select2-number_room-container");
-            this.mouse.click("#select2-number_room-results li:first-child");
-
-            this.mouse.click("#spaceheat-appl-group input[value='none']");
-            this.mouse.click("#cloth-dryer-group label[for='dryer-no']");
-
-            this.mouse.click("#water-heating-group input[value='other']");
-
-            this.click(".profile-btn");
-        });
-
-        this.wait(5000, function() {
-            this.thenOpen("https://compare.switchon.vic.gov.au/service/offers", {
-                method: 'get',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json'
-                }
-            });
-        });
-
-        this.then(function(){
-            var data = this.getPageContent().replace(/<\/?[^>]+(>|$)/g, ""),
-                json = JSON.parse(data),
-                offers = json["offersList"];
-
-            this.each(offers, function(self, offer){
-                offerList.push(offer);
+            this.wait(5000, function() {
+                this.click("label[for='energy-concession-no']");
+                this.click("#disclaimer_chkbox");
+                this.click("#btn-proceed");
             });
 
-            casper.echo(offerList.length);
+            this.wait(5000, function() {
+                this.mouse.click("#select2-number_person-container");
+                this.mouse.click("#select2-number_person-results li:first-child");
+
+                this.mouse.click("#select2-number_room-container");
+                this.mouse.click("#select2-number_room-results li:first-child");
+
+                this.mouse.click("#spaceheat-appl-group input[value='none']");
+                this.mouse.click("#cloth-dryer-group label[for='dryer-no']");
+
+                this.mouse.click("#water-heating-group input[value='other']");
+
+                this.click(".profile-btn");
+            });
+
+            this.wait(5000, function() {
+                this.thenOpen("https://compare.switchon.vic.gov.au/service/offers", {
+                    method: 'get',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Accept': 'application/json'
+                    }
+                });
+            });
+
+            this.wait(5000, function(){
+                var data = this.getPageContent().replace(/<\/?[^>]+(>|$)/g, ""),
+                    json = JSON.parse(data),
+                    offers = json["offersList"];
+
+                this.each(offers, function(self, offer){
+                    offerList.push(offer);
+                });
+
+                casper.thenOpen(url, function(){
+                });
+            });
+
+
         });
-   });
+    })(current);
+    current++;
+}
+
 casper.run();
+(offerList);
--- sample3.js
+++ sample3.js
@@ -1,64 +1,127 @@
 /**
  * Created by binhtd on 25/08/2016.
  */
-var casper = require("casper").create();
+//var casper = require("casper").create();
+//
+//casper.echo("Casper CLI passed args:");
+//require("utils").dump(casper.cli.args);
+//
+//casper.echo("Casper CLI passed options:");
+//require("utils").dump(casper.cli.options);
+//
+//
+//casper.echo(casper.cli.has(0));
+//casper.echo(casper.cli.get(0));
+//casper.echo(casper.cli.has(3));
+//casper.echo(casper.cli.get(3));
+//casper.echo(casper.cli.has("foo"));
+//casper.echo(casper.cli.get("foo"));
+//casper.cli.drop("foo");
+//casper.echo(casper.cli.has("foo"));
+//casper.echo(casper.cli.get("foo"));
+//
+//
+//casper.start("http://domain.tld/page.html", function(){
+//   if (this.exists("h1.page-title")){
+//       this.echo("the heading exists");
+//   }
+//});
+//
+//casper.run();
+//
+//casper.exit();
+//
+//
+//casper.then(function() {
+//    links = this.evaluate(getLinks);
+//    var j = 0;
+//    this.eachThen(links,function(response){
+//        j++;
+//        if(j >= maxLinks) return;
+//
+//        this.thenOpen(response.data, function writeOnJson() {
+//
+//            var objectsCount = this.evaluate(function(){
+//                return __utils__.findAll('.list .quote p').length;
+//            });
+//
+//            var objects = this.evaluate(function(){
+//                return __utils__.findAll('.list .quote p');
+//            });
+//
+//            quotes = new Array();
+//            if(objectsCount != undefined) {
+//                for(i = 0; i < objectsCount; i++) {
+//                    if(objects[i] != null) {
+//                        var quote = new Quote(objects[i]['innerText']);
+//                        quotes.push(quote);
+//                    }
+//                }
+//            }
+//        });
+//    });
+//
+//});
 
-casper.echo("Casper CLI passed args:");
-require("utils").dump(casper.cli.args);
-
-casper.echo("Casper CLI passed options:");
-require("utils").dump(casper.cli.options);
-
-
-casper.echo(casper.cli.has(0));
-casper.echo(casper.cli.get(0));
-casper.echo(casper.cli.has(3));
-casper.echo(casper.cli.get(3));
-casper.echo(casper.cli.has("foo"));
-casper.echo(casper.cli.get("foo"));
-casper.cli.drop("foo");
-casper.echo(casper.cli.has("foo"));
-casper.echo(casper.cli.get("foo"));
+//var coupons = [[1, 2], [2, 3], [3, 4]]; // fake values for testing
+//var casper = require('casper').create();
+//casper.start();
+//
+//casper.then(function() {
+//    this.eachThen(coupons, function(response) {
+//        console.log(JSON.stringify(response.data));
+//    });
+//});
+//
+//casper.run();
 
 
-casper.start("http://domain.tld/page.html", function(){
-   if (this.exists("h1.page-title")){
-       this.echo("the heading exists");
-   }
+var casper = require('casper').create({
+        waitTimeout: 300000 //new maximum timeout
+        // pageSettings: {
+        //   loadImages:  false // disable to load img
+        // }
+    }),
+    x = require('casper').selectXPath;
+var page = require('webpage').create();
+var gasHomePostcodeList = [3011, 3953, 3179, 3141, 3199];
+
+casper.start('https://www.google..com/', function() {
 });
-
-casper.run();
-
-casper.exit();
-
 
 casper.then(function() {
-    links = this.evaluate(getLinks);
-    var j = 0;
-    this.eachThen(links,function(response){
-        j++;
-        if(j >= maxLinks) return;
+    var current = 0;
+    var end = gasHomePostcodeList.length - 1;
 
-        this.thenOpen(response.data, function writeOnJson() {
+    for (;current < end;) {
 
-            var objectsCount = this.evaluate(function(){
-                return __utils__.findAll('.list .quote p').length;
+        (function(cntr) {
+            casper.then(function() {
+                this.echo('casper.async: '+ gasHomePostcodeList[cntr]);
+                // here we can download stuff
             });
+        })(current);
 
-            var objects = this.evaluate(function(){
-                return __utils__.findAll('.list .quote p');
-            });
+        current++;
 
-            quotes = new Array();
-            if(objectsCount != undefined) {
-                for(i = 0; i < objectsCount; i++) {
-                    if(objects[i] != null) {
-                        var quote = new Quote(objects[i]['innerText']);
-                        quotes.push(quote);
-                    }
-                }
-            }
-        });
-    });
+    }
 
 });
+
+//casper.then(function() {
+//    var current = 5;
+//    while (current > 1) {
+//
+//        (function(cntr){
+//            casper.then(function(){
+//                this.echo('casper.async: '+cntr);
+//            });
+//        })(current);
+//
+//        current--;
+//    }
+//});
+
+
+
+casper.run();
