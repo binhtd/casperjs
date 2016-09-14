11f2159c5a579aaf0e99e00ebf2351412e0ceed1
binhtd <trinhducbinh@gmail.com>
2016-09-14 10:32:34 +0700

    update

--- start.sh
+++ start.sh
@@ -1,84 +1,85 @@
 #! /bin/bash
 
 echo "#####################################################################"
-echo "casperjs crawler2.js --fueltype=gas --businesstype=home --postcode=3011;"
+ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9;
+echo "casperjs crawler2.js --fueltype=gas --businesstype=home --postcode=3011;";
 casperjs crawler2.js --fueltype=gas --businesstype=home --postcode=3011;
 
-ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9
-echo "casperjs crawler2.js --fueltype=gas --businesstype=home --postcode=3953;"
+ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9;
+echo "casperjs crawler2.js --fueltype=gas --businesstype=home --postcode=3953;";
 casperjs crawler2.js --fueltype=gas --businesstype=home --postcode=3953;
 
-ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9
-echo "casperjs crawler2.js --fueltype=gas --businesstype=home --postcode=3179;"
+ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9;
+echo "casperjs crawler2.js --fueltype=gas --businesstype=home --postcode=3179;";
 casperjs crawler2.js --fueltype=gas --businesstype=home --postcode=3179;
 
-ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9
-echo "casperjs crawler2.js --fueltype=gas --businesstype=home --postcode=3141;"
+ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9;
+echo "casperjs crawler2.js --fueltype=gas --businesstype=home --postcode=3141;";
 casperjs crawler2.js --fueltype=gas --businesstype=home --postcode=3141;
 
-ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9
-echo "casperjs crawler2.js --fueltype=gas --businesstype=home --postcode=3199;"
+ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9;
+echo "casperjs crawler2.js --fueltype=gas --businesstype=home --postcode=3199;";
 casperjs crawler2.js --fueltype=gas --businesstype=home --postcode=3199;
 
 
-ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9
-echo "casperjs crawler2.js --fueltype=gas --businesstype=business --postcode=3011;"
+ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9;
+echo "casperjs crawler2.js --fueltype=gas --businesstype=business --postcode=3011;";
 casperjs crawler2.js --fueltype=gas --businesstype=business --postcode=3011;
 
-ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9
-echo "casperjs crawler2.js --fueltype=gas --businesstype=business --postcode=3953;"
+ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9;
+echo "casperjs crawler2.js --fueltype=gas --businesstype=business --postcode=3953;";
 casperjs crawler2.js --fueltype=gas --businesstype=business --postcode=3953;
 
-ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9
-echo "casperjs crawler2.js --fueltype=gas --businesstype=business --postcode=3179;"
+ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9;
+echo "casperjs crawler2.js --fueltype=gas --businesstype=business --postcode=3179;";
 casperjs crawler2.js --fueltype=gas --businesstype=business --postcode=3179;
 
-ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9
-echo "casperjs crawler2.js --fueltype=gas --businesstype=business --postcode=3141;"
+ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9;
+echo "casperjs crawler2.js --fueltype=gas --businesstype=business --postcode=3141;";
 casperjs crawler2.js --fueltype=gas --businesstype=business --postcode=3141;
 
-ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9
-echo "casperjs crawler2.js --fueltype=gas --businesstype=business --postcode=3199;"
+ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9;
+echo "casperjs crawler2.js --fueltype=gas --businesstype=business --postcode=3199;";
 casperjs crawler2.js --fueltype=gas --businesstype=business --postcode=3199;
 
-ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9
-echo "casperjs crawler2.js --fueltype=electric --businesstype=home --postcode=3000;"
+ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9;
+echo "casperjs crawler2.js --fueltype=electric --businesstype=home --postcode=3000;";
 casperjs crawler2.js --fueltype=electric --businesstype=home --postcode=3000;
 
-ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9
-echo "casperjs crawler2.js --fueltype=electric --businesstype=home --postcode=3011;"
+ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9;
+echo "casperjs crawler2.js --fueltype=electric --businesstype=home --postcode=3011;";
 casperjs crawler2.js --fueltype=electric --businesstype=home --postcode=3011;
 
-ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9
-echo "casperjs crawler2.js --fueltype=electric --businesstype=home --postcode=3944;"
+ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9;
+echo "casperjs crawler2.js --fueltype=electric --businesstype=home --postcode=3944;";
 casperjs crawler2.js --fueltype=electric --businesstype=home --postcode=3944;
 
-ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9
-echo "casperjs crawler2.js --fueltype=electric --businesstype=home --postcode=3284;"
+ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9;
+echo "casperjs crawler2.js --fueltype=electric --businesstype=home --postcode=3284;";
 casperjs crawler2.js --fueltype=electric --businesstype=home --postcode=3284;
 
-ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9
-echo "casperjs crawler2.js --fueltype=electric --businesstype=home --postcode=3841;"
+ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9;
+echo "casperjs crawler2.js --fueltype=electric --businesstype=home --postcode=3841;";
 casperjs crawler2.js --fueltype=electric --businesstype=home --postcode=3841;
 
 
-ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9
-echo "casperjs crawler2.js --fueltype=electric --businesstype=business --postcode=3000;"
+ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9;
+echo "casperjs crawler2.js --fueltype=electric --businesstype=business --postcode=3000;";
 casperjs crawler2.js --fueltype=electric --businesstype=business --postcode=3000;
 
-ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9
-echo "casperjs crawler2.js --fueltype=electric --businesstype=business --postcode=3011;"
+ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9;
+echo "casperjs crawler2.js --fueltype=electric --businesstype=business --postcode=3011;";
 casperjs crawler2.js --fueltype=electric --businesstype=business --postcode=3011;
 
-ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9
-echo "casperjs crawler2.js --fueltype=electric --businesstype=business --postcode=3944;"
+ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9;
+echo "casperjs crawler2.js --fueltype=electric --businesstype=business --postcode=3944;";
 casperjs crawler2.js --fueltype=electric --businesstype=business --postcode=3944;
 
-ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9
-echo "casperjs crawler2.js --fueltype=electric --businesstype=business --postcode=3284;"
+ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9;
+echo "casperjs crawler2.js --fueltype=electric --businesstype=business --postcode=3284;";
 casperjs crawler2.js --fueltype=electric --businesstype=business --postcode=3284;
 
-ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9
-echo "casperjs crawler2.js --fueltype=electric --businesstype=business --postcode=3841;"
+ps aux | grep -i phantomjs | awk {'print $2'} | xargs kill -9;
+echo "casperjs crawler2.js --fueltype=electric --businesstype=business --postcode=3841;";
 casperjs crawler2.js --fueltype=electric --businesstype=business --postcode=3841;
 echo "#####################################################################"
